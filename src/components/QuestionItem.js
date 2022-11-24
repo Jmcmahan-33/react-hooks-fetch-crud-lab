106396541ab0ca0188c6df6226f9c26ad4fd70dc
Jared McMahan <jaredm788@gmail.com>
2022-11-24 16:37:28 -0700

    commit tree e347b1150cec200a69165e69eba9ebefb838693b

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteClick}) {
   const { id, prompt, answers, correctIndex } = question;
+
+ 
+
+  function handleDeleteClick() {
+    onDeleteClick(id);
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -17,7 +23,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,45 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+//Get Request for data to load 
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(response => response.json())
+    .then(data => setQuestions(data))
+  }, [])
+
+
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((q) => q.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+
+
+
+  // map returns each individual object in the array 
+  const questionItems = questions.map((q) => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+      onDeleteClick={handleDeleteClick}
+    
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
